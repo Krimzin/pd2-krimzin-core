84f1dc1401509ea4cfc1159a81a6cd5e5ab51453
Krimzin <email.krimzin@gmail.com>
2022-07-06 22:35:06 +1200

    Load localization with require

--- krimzin-core/lua/hooks/pre/Entry.lua
+++ krimzin-core/lua/hooks/pre/Entry.lua
@@ -1,5 +1,6 @@
+local ModPath = ModPath
 KrimzinCore = setmetatable({}, {__index = _G})
 KrimzinCore.MOD_PATH = ModPath
 
 dofile(ModPath .. "lua/core/Functions.lua")
-dofile(ModPath .. "lua/core/Localization.lua")
+KrimzinCore.require(ModPath .. "lua/core/Localization.lua")
