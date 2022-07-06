95929d4a8fbda58dbf9ce6aa4d5f4c83567f74a6
Krimzin <email.krimzin@gmail.com>
2022-07-06 19:39:04 +1200

    Load lua/core/Localization.lua on Entry

--- krimzin-core/lua/hooks/post/LocalizationManager.lua
+++ krimzin-core/lua/hooks/post/LocalizationManager.lua
@@ -1,7 +1,4 @@
-KrimzinCore.require(ModPath .. "lua/core/Localization.lua")
-
 local localization_path = ModPath .. "localization/"
-
 Hooks:Add("LocalizationManagerPostInit", "KrimzinCore.load_localization", function (self)
 	KrimzinCore.load_localization(self, localization_path)
 end)
--- krimzin-core/lua/hooks/pre/Entry.lua
+++ krimzin-core/lua/hooks/pre/Entry.lua
@@ -2,3 +2,4 @@ KrimzinCore = setmetatable({}, {__index = _G})
 KrimzinCore.MOD_PATH = ModPath
 
 dofile(ModPath .. "lua/core/Functions.lua")
+dofile(ModPath .. "lua/core/Localization.lua")
